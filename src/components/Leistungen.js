434cd2b8f9782629e766d5794d426c1d88a8dfd4
Nils Home <nils.kummert@iqdigital.de>
2020-08-14 12:39:43 +0200

    MOD - implemented first images

--- src/components/Leistungen.js
+++ src/components/Leistungen.js
@@ -1,24 +1,30 @@
 import React, {useState} from "react";
 import "./leistungen.css";
 import NeuanlageImage from "./../images/neuanlage.png"
+import PflasterImage from "./../images/pflaster.png"
+import PflegeImage from "./../images/pflege.png"
+import TeichImage from "./../images/teich.png"
+import TerasseImage from "./../images/terasse.png"
+import ZaunImage from "./../images/zaun.png"
 import { useTransition, useSpring,  animated } from 'react-spring'
 
 const Leistungen = () => {
     const [selectedItem, setSelectedItem] = useState(0);
     const listItems = ["Neuanlage", "Pflasterarbeiten", "Holzterassen", "Einzäungen", "Pflegearbeiten", "Teichanlagen"];
+    const listImages = [NeuanlageImage, PflasterImage, TerasseImage, ZaunImage, PflegeImage, TeichImage];
     const paragraph = "Enim ex aliquip magna eu duis. Proident proident ad excepteur proident adipisicing commodo. Eu ad culpa nostrud pariatur fugiat tempor. Sunt qui in anim consequat culpa mollit velit commodo pariatur ipsum veniam.";
     const transitionItems = [
-        ({style}) => <LeistungItem style={{...style}} title={listItems[0]} paragraph={paragraph}/>,
-        ({style}) => <LeistungItem style={{...style}} title={listItems[1]} paragraph={paragraph}/>,
-        ({style}) => <LeistungItem style={{...style}} title={listItems[2]} paragraph={paragraph}/>,
-        ({style}) => <LeistungItem style={{...style}} title={listItems[3]} paragraph={paragraph}/>,
-        ({style}) => <LeistungItem style={{...style}} title={listItems[4]} paragraph={paragraph}/>,
-        ({style}) => <LeistungItem style={{...style}} title={listItems[5]} paragraph={paragraph}/>,
+        ({style}) => <LeistungItem style={{...style}} title={listItems[0]} paragraph={paragraph} image={listImages[0]}/>,
+        ({style}) => <LeistungItem style={{...style}} title={listItems[1]} paragraph={paragraph} image={listImages[1]}/>,
+        ({style}) => <LeistungItem style={{...style}} title={listItems[2]} paragraph={paragraph} image={listImages[2]}/>,
+        ({style}) => <LeistungItem style={{...style}} title={listItems[3]} paragraph={paragraph} image={listImages[3]}/>,
+        ({style}) => <LeistungItem style={{...style}} title={listItems[4]} paragraph={paragraph} image={listImages[4]}/>,
+        ({style}) => <LeistungItem style={{...style}} title={listItems[5]} paragraph={paragraph} image={listImages[5]}/>,
     ]
     const transitions = useTransition(selectedItem, p => p, {
-        from: { opacity: 0, transform: 'translate3d(0,100%,0)' , position: "absolute"},
+        from: { opacity: 0, transform: 'translate3d(100%,0,0)' , position: "absolute"},
         enter: { opacity: 1, transform: 'translate3d(0%,0,0)' , position: "relative"},
-        leave: { opacity: 0, transform: 'translate3d(0,-100%,0)', position: "absolute",display: "none" },
+        leave: { opacity: 0, transform: 'translate3d(-100%,0,0)', position: "absolute",display: "none" },
     })
     const listItemOnClickHandler = (i) =>{
         return () => setSelectedItem(i);
@@ -63,7 +69,7 @@ const LeistungBar = ({listItems, selectedItem, listItemOnClickHandler}) =>{
     )
 }
 
-const LeistungItem = ({paragraph, style}) =>{
+const LeistungItem = ({paragraph, style, image}) =>{
     return(
         <animated.div style={style} className="component_leistungItem">
             <div className="leistungItem_5050">
@@ -71,7 +77,7 @@ const LeistungItem = ({paragraph, style}) =>{
                     <p>{paragraph}</p>
                 </div>
                 <div className="leistungItem_5050_content right">
-                    <img src={NeuanlageImage}/>
+                    <img src={image}/>
                 </div>
             </div>
         </animated.div>
